886e107f322bf541791040e42014da3f6c804d07
Gary Servin <garyservin@gmail.com>
2014-02-15 00:18:21 -0300

    Add keyboard teleop based on turtlesim_teleop

--- src/ros_hase_key_node.cpp
+++ src/ros_hase_key_node.cpp
@@ -1,35 +1,129 @@
-#include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <signal.h>
+#include <termios.h>
+#include <stdio.h>
 
-#include <sstream>
+#define KEYCODE_R 0x43
+#define KEYCODE_L 0x44
+#define KEYCODE_U 0x41
+#define KEYCODE_D 0x42
+#define KEYCODE_Q 0x71
 
-int main(int argc, char **argv)
+class TeleopHase
 {
-  ros::init(argc, argv, "talker");
+public:
+  TeleopHase();
+  void keyLoop();
+private:
+  ros::NodeHandle nh_;
+  double linear_, angular_, l_scale_, a_scale_;
+  ros::Publisher twist_pub_;
+};
 
-  ros::NodeHandle n;
-  ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 10);
+TeleopHase::TeleopHase():
+  linear_(0),
+  angular_(0),
+  l_scale_(0.7),
+  a_scale_(0.7)
+{
+  nh_.param("scale_angular", a_scale_, a_scale_);
+  nh_.param("scale_linear", l_scale_, l_scale_);
 
-  ros::Rate loop_rate(10);
+  twist_pub_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+}
 
-  int count = 0;
-  while (ros::ok())
+int kfd = 0;
+struct termios cooked, raw;
+
+void quit(int sig)
+{
+  tcsetattr(kfd, TCSANOW, &cooked);
+  ros::shutdown();
+  exit(0);
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "teleop_hase");
+  TeleopHase teleop_hase;
+
+  signal(SIGINT,quit);
+
+  teleop_hase.keyLoop();
+
+  return(0);
+}
+
+
+void TeleopHase::keyLoop()
+{
+  char c;
+  bool dirty=false;
+
+
+  // get the console in raw mode
+  tcgetattr(kfd, &cooked);
+  memcpy(&raw, &cooked, sizeof(struct termios));
+  raw.c_lflag &=~ (ICANON | ECHO);
+  // Setting a new line, then end of file
+  raw.c_cc[VEOL] = 1;
+  raw.c_cc[VEOF] = 2;
+  tcsetattr(kfd, TCSANOW, &raw);
+
+  puts("Reading from keyboard");
+  puts("---------------------------");
+  puts("Use arrow keys to move the hase.");
+
+
+  for(;;)
   {
-    geometry_msgs::Twist vel;
+    // get the next event from the keyboard
+    if(read(kfd, &c, 1) < 0)
+    {
+      perror("read():");
+      exit(-1);
+    }
 
-    vel.angular.z = 0.0;
-    vel.linear.x = 1.0 ;
+    linear_=angular_=0;
+    ROS_DEBUG("value: 0x%02X\n", c);
 
-    //ROS_INFO("%", .data.c_str());
-    ROS_DEBUG("[i] forward %.2f %.2f", vel.linear.x, vel.angular.z);
+    switch(c)
+    {
+      case KEYCODE_L:
+        ROS_DEBUG("LEFT");
+        angular_ = 1.0;
+        dirty = true;
+        break;
+      case KEYCODE_R:
+        ROS_DEBUG("RIGHT");
+        angular_ = -1.0;
+        dirty = true;
+        break;
+      case KEYCODE_U:
+        ROS_DEBUG("UP");
+        linear_ = 1.0;
+        dirty = true;
+        break;
+      case KEYCODE_D:
+        ROS_DEBUG("DOWN");
+        linear_ = -1.0;
+        dirty = true;
+        break;
+      case KEYCODE_Q:
+        ROS_DEBUG("STOP");
+        dirty = true;
+        break;
+    }
 
-    vel_pub.publish(vel);
-
-    ros::spinOnce();
-
-    loop_rate.sleep();
-    ++count;
+    geometry_msgs::Twist twist;
+    twist.angular.z = a_scale_*angular_;
+    twist.linear.x = l_scale_*linear_;
+    if(dirty ==true)
+    {
+      twist_pub_.publish(twist);
+      dirty=false;
+    }
   }
-
-  return 0;
+  return;
 }
