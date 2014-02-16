e36c4e20b26fd18f35be20e3e0f35e3036ed51f0
Gary Servin <garyservin@gmail.com>
2014-02-15 21:22:40 -0300

    Fixed reverse driving due to uint defined variable

--- include/hase/MoveRobot.h
+++ include/hase/MoveRobot.h
@@ -9,13 +9,6 @@
 #include "SimpleGPIO.h"
 #include "SimplePWM.h"
 
-#define RIGHT   0
-#define LEFT    1
-#define FORWARD 2
-#define REVERSE 3
-#define STOP    4
-#define BRAKE   5
-
 #define R_FWD   69   // GPIO1_13 = (1x32) + 13 = 45
 #define R_REV   45   // GPIO2_5  = (2x32) +  5 = 69
 #define R_PWM   23   // GPIO0_23 = (0x32) + 23 = 23
@@ -29,8 +22,6 @@
 
 int init_robot();
 int close_robot();
-int move_robot(int direction, float speed);
 int move_robot_2WD(double left, double right);
-int rotate_motor(int motor, int direction, float speed);
 
 #endif /* MOVEROBOT_H_ */
--- src/hase/MoveRobot.cpp
+++ src/hase/MoveRobot.cpp
@@ -32,40 +32,14 @@ int close_robot(){
     return 0;
 }
 
-int move_robot(int direction, float speed){
-    switch(direction){
-        case FORWARD:
-            rotate_motor(RIGHT, FORWARD, speed);
-            rotate_motor(LEFT, FORWARD, speed);
-            break;
-        case REVERSE:
-            rotate_motor(RIGHT, REVERSE, speed);
-            rotate_motor(LEFT, REVERSE, speed);
-            break;
-        case LEFT:
-            rotate_motor(RIGHT, FORWARD, speed);
-            rotate_motor(LEFT, REVERSE, speed);
-            break;
-        case RIGHT:
-            rotate_motor(RIGHT, REVERSE, speed);
-            rotate_motor(LEFT, FORWARD, speed);
-            break;
-        case STOP:
-            rotate_motor(RIGHT, STOP, speed);
-            rotate_motor(LEFT, STOP, speed);
-            break;
-    }
-    return 0;
-}
-
 int move_robot_2WD(double left, double right){
     PIN_VALUE l_forward = LOW;
     PIN_VALUE l_reverse = LOW;
     PIN_VALUE r_forward = LOW;
     PIN_VALUE r_reverse = LOW;
 
-    uint16_t l_pwm = (int)(left * 50000);
-    uint16_t r_pwm = (int)(right * 50000);
+    int l_pwm = (int)(left * 50000);
+    int r_pwm = (int)(right * 50000);
 
     if (right > 0){
         r_forward = HIGH;
@@ -83,51 +57,13 @@ int move_robot_2WD(double left, double right){
         l_reverse = HIGH;
     }
 
-    gpio_set_value(R_FWD, l_forward);
-    gpio_set_value(R_REV, l_reverse);
+    gpio_set_value(R_FWD, r_forward);
+    gpio_set_value(R_REV, r_reverse);
     set_duty_cycle(0, r_pwm);
 
-    gpio_set_value(L_FWD, r_forward);
-    gpio_set_value(L_REV, r_reverse);
+    gpio_set_value(L_FWD, l_forward);
+    gpio_set_value(L_REV, l_reverse);
     set_duty_cycle(1, l_pwm);
 
-    return 0;
-}
-
-int rotate_motor(int motor, int direction, float speed){
-    PIN_VALUE forward = LOW;
-    PIN_VALUE reverse = LOW;
-    int pwm = 0;
-
-    switch(direction){
-        case FORWARD:
-            forward = HIGH;
-            reverse = LOW;
-            break;
-        case REVERSE:
-            forward = LOW;
-            reverse = HIGH;
-            break;
-        case STOP:
-            forward = LOW;
-            reverse = LOW;
-            break;
-        case BRAKE:
-            forward = HIGH;
-            reverse = HIGH;
-            break;
-    }
-
-    pwm = (int)(speed * 50000);
-
-    if (motor == LEFT){
-        gpio_set_value(L_FWD, forward);
-        gpio_set_value(L_REV, reverse);
-        set_duty_cycle(1, pwm);
-    }else{
-        gpio_set_value(R_FWD, forward);
-        gpio_set_value(R_REV, reverse);
-        set_duty_cycle(0, pwm);
-    }
     return 0;
 }
